f0632ff343df3c1461f8343ea946ff40309f4e26
miaomiao1992 <644284807@qq.com>
2022-08-28 15:44:46 +0800

    20220828-push

---
+++ git-push.bat
@@ -0,0 +1,12 @@
+@echo off
+set "Ymd=%date:~,4%%date:~5,2%%date:~8,2%-push"
+
+ git add src
+ git add git-cookbook
+ git add  README.md LICENSE
+ git add git-push.bat 
+ git commit -m  %Ymd%
+ git push origin main
+
+echo git-push success!
+@pause
